19adbcef239cb172d8514ea046dca6481dc983d3
Jason0411202 <Jason0411202@gmail.com>
2024-05-17 01:26:26 +0800

    包裝成 discord

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import time
 import mysql.connector
 import twstock
+import discord
+from discord.ext import tasks
+from dotenv import load_dotenv
+import os
+
+intents = discord.Intents.default() #intents 是要求的權限
+intents.message_content = True
+client = discord.Client(intents=intents) #client是與discord連結的橋樑
+load_dotenv()
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+BOT_API_KEY = os.getenv("BOT_API_KEY")
 
 def ConnectToDatabase():
     print("ConnectToDatabase")
@@ -58,7 +70,7 @@ def CheckStockBuyTime(conn, stock_id): # 檢查是否在過去一個禮拜內買
     return 0
         
 
-def Buy(conn, allInfo, stock_id, reason):
+async def Buy(conn, allInfo, stock_id, reason):
 
     if CheckStockBuyTime(conn, stock_id) == 1: # 過去一個禮拜內買過了
         return # 暫時不買
@@ -73,6 +85,8 @@ def Buy(conn, allInfo, stock_id, reason):
     cursor.execute(insertCommand, (stock_id, myMoney/20, stockPrice, nowTime, reason)) # 執行 SQL 插入，每次都花 1/20 的本金購買股票
     conn.commit() # 提交 SQL 插入
     print("買入股票: ", stock_id, ", 購入股價: ", stockPrice, ", 購入總金額: ", myMoney/20, ", 購入原因: ", reason)
+    channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
+    await channel.send("買入股票: "+str(stock_id)+"\n購入股價: "+str(stockPrice)+"\n購入總金額: "+str(myMoney/20)+"\n購入原因: "+str(reason)+"\n---")
 
     AlterMyMoney(conn, myMoney-myMoney/20) # 呼叫 AlterMyMoney 函式
 
@@ -92,7 +106,7 @@ def REMEMBER_SELL_HISTORY(stock_id, amount, buyPrice, sellPrice, profit, reason)
     conn.close() # 關閉連線
 
 
-def Sell(conn, allInfo, stock_id, reason):
+async def Sell(conn, allInfo, stock_id, reason):
     myMoney=CheckMyMoney(conn) # 呼叫 CheckMyMoney 函式
     cursor = conn.cursor() # 建立 cursor 物件
 
@@ -102,6 +116,7 @@ def Sell(conn, allInfo, stock_id, reason):
 
     if len(rows) == 0: # 如果查詢結果為空
         print("目前未持有此股票, 股號:", stock_id)
+
         return # 沒得賣
 
     final_profit=0
@@ -112,6 +127,8 @@ def Sell(conn, allInfo, stock_id, reason):
         profit=(float(sellPrice)/float(buyPrice))*float(amount) - float(amount) # 計算損益
         profitRate=(float(sellPrice)/float(buyPrice))-1 # 計算損益率
         print("股號:", stock_id, ", 買入價格:", buyPrice, ", 賣出價格:", sellPrice, ", 已實現損益:", profit, ", 已實現損益率:", profitRate*100, "%", ", 賣出原因:", reason)
+        channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
+        await channel.send("股號: "+str(stock_id)+"\n買入價格: "+str(buyPrice)+"\n賣出價格: "+str(sellPrice)+"\n已實現損益: "+str(profit)+"\n已實現損益率: "+str(profitRate*100)+"%\n賣出原因: "+str(reason)+"\n---")
 
         REMEMBER_SELL_HISTORY(stock_id, amount, buyPrice, sellPrice, profit, reason)
 
@@ -124,7 +141,7 @@ def Sell(conn, allInfo, stock_id, reason):
 
     AlterMyMoney(conn, myMoney+final_profit) # 呼叫 AlterMyMoney 函式
 
-def Check(conn, allInfo):
+async def Check(conn, allInfo):
     cursor = conn.cursor() # 建立 cursor 物件
     cursor.execute("SELECT * FROM MYSTOCK") # 執行 SQL 查詢
     rows = cursor.fetchall() # 擷取所有的資料
@@ -140,6 +157,9 @@ def Check(conn, allInfo):
             profit=(float(sellPrice)/float(buyPrice))*float(amount) - float(amount) # 計算損益
             profitRate=(float(sellPrice)/float(buyPrice))-1 # 計算損益率
             print("股號:", stock_id, ", 買入價格:", buyPrice, ", 當前價格:", sellPrice, ", 購買的總金額數:", amount, ", 未實現損益:", profit, ", 未實現損益率:", profitRate*100, "%", ", 購入原因:", reason)
+            channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
+            await channel.send("股號: "+str(stock_id)+"\n買入價格: "+str(buyPrice)+"\n當前價格: "+str(sellPrice)+"\n購買的總金額數: "+str(amount)+"\n未實現損益: "+str(profit)+"\n未實現損益率: "+str(profitRate*100)+"%\n購入原因: "+str(reason)+"\n---")
+
         except Exception as e:
             print(e)
 
@@ -154,9 +174,11 @@ def Check(conn, allInfo):
         total_profit+=row[0]
     
     print("歷史總損益:", total_profit)
+    channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
+    await channel.send("---\n歷史總損益: "+str(total_profit)+"\n---")
 
 
-def RealTime_GET():
+def RealTime_GET(conn):
     cursor = conn.cursor() # 建立 cursor 物件
     cursor.execute("SELECT stockID FROM STOCK") # 執行 SQL 查詢
     rows = cursor.fetchall() # 擷取所有的資料
@@ -167,9 +189,16 @@ def RealTime_GET():
     
     return twstock.realtime.get(stockID_list)
 
-def MainProcess(conn):
+@tasks.loop(seconds=1.0) #每60秒執行一次
+async def Time_Check():
+    # 如果現在時間不是 14:00 整，就不執行
+    if time.localtime().tm_hour != 1 or time.localtime().tm_min != 25:
+        return
+
+
+    conn=ConnectToDatabase() # 呼叫 ConnectToDatabase 函式
     ############################################################ 短線交易邏輯 ############################################################
-    allInfo=RealTime_GET()
+    allInfo=RealTime_GET(conn)
     print(allInfo)
 
     cursor = conn.cursor() # 建立 cursor 物件
@@ -181,7 +210,7 @@ def MainProcess(conn):
             bfp = twstock.BestFourPoint(stock) # 建立 BestFourPoint 物件
             result, reason = bfp.best_four_point() # 取得最佳四大買賣點 (以上次收盤時的資料為準)
             if not result and reason.count(',')>=1: # 如果符合最佳四大買賣點，且有兩個以上的理由
-                Sell(conn, allInfo, row[0], reason) # 呼叫 Sell 函式
+                await Sell(conn, allInfo, row[0], reason) # 呼叫 Sell 函式
                 print("\n")
 
         except Exception as e:
@@ -193,20 +222,24 @@ def MainProcess(conn):
             bfp = twstock.BestFourPoint(stock) # 建立 BestFourPoint 物件
             result, reason = bfp.best_four_point() # 取得最佳四大買賣點 (以上次收盤時的資料為準)
             if result and reason.count(',')>=1: # 如果符合最佳四大買賣點
-                Buy(conn, allInfo, row[0], reason) # 呼叫 Buy 函式
+                await Buy(conn, allInfo, row[0], reason) # 呼叫 Buy 函式
                 print("\n")
 
         except Exception as e:
             print(e)
 
     # 確認當前持股資料
-    Check(conn, allInfo)
+    await Check(conn, allInfo)
 
     ############################################################ 短線交易邏輯 ############################################################
     # 關閉 cursor 和連線
     cursor.close()
     conn.close()
 
+@client.event
+async def on_ready(): #啟動成功時會呼叫
+    channel = discord.utils.get(client.get_all_channels(), id=int(CHANNEL_ID))
+    await channel.send("啟動")
+    Time_Check.start() #每60秒在背景執行Time_Check函式
 
-conn=ConnectToDatabase() # 呼叫 ConnectToDatabase 函式
-MainProcess(conn) # 呼叫主程式
+client.run(BOT_API_KEY) #啟動bot
